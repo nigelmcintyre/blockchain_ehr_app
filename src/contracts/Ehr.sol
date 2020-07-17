ec216e44a4e71fc39f2a656222a94aeca0f61448
Nigel McIntyre <nigelmcintyre1995@gmail.com>
2020-07-17 11:42:52 +0100

    added access control to smart contract and tested it

--- src/contracts/Ehr.sol
+++ src/contracts/Ehr.sol
@@ -10,6 +10,7 @@ contract Ehr is AccessControl{
     constructor () public {
         _setupRole(DEFAULT_ADMIN_ROLE, root);
         _setRoleAdmin(DOCTOR_ROLE, DEFAULT_ADMIN_ROLE);
+        _setRoleAdmin(PATIENT_ROLE, DOCTOR_ROLE);
     }
 
     modifier onlyAdmin() {
@@ -18,8 +19,27 @@ contract Ehr is AccessControl{
     }
 
     modifier onlyDoctor() {
-      require(isDoctor(msg.sender), "Restricted to Doctors.");
+      require(isDoctor(msg.sender), "Restricted to doctors.");
       _;
+    }
+
+    modifier onlyDoctorPatient(){
+    
+      require(isUser(msg.sender), "Restricted to registered users");
+     
+      _;
+    }
+    // To allow doctors and the patient who owns the record access their data
+    function isUser(address account) public virtual view returns (bool) {
+      if(hasRole(DOCTOR_ROLE, account)){
+        return true;
+      } else if(hasRole(PATIENT_ROLE, account)){
+        return true;
+      } else if(hasRole(DEFAULT_ADMIN_ROLE, account)) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
     function isAdmin(address account) public virtual view returns (bool) {
@@ -30,8 +50,16 @@ contract Ehr is AccessControl{
       return hasRole(DOCTOR_ROLE, account);
     }
 
+    function isPatient(address account) public virtual view returns (bool) {
+      return hasRole(PATIENT_ROLE, account);
+    }
+
     function addDoctorRole(address account) public virtual onlyAdmin {
       grantRole(DOCTOR_ROLE, account);
+    }
+
+    function addPatientRole(address account) public virtual onlyDoctor {
+      grantRole(PATIENT_ROLE, account);
     }
 
     struct PatientDetails {
@@ -57,39 +85,42 @@ contract Ehr is AccessControl{
         string calldata _name,
         string calldata _email,
         string calldata _password,
-        string calldata _patientHash) external {
+        string calldata _patientHash) external onlyDoctor {
         patients[_address] = PatientDetails(_address,
                                             _name,
                                             _email,
                                             _password,
                                             _patientHash); 
+        addPatientRole(_address);
     }
 
     function newDoctor(
         address _address,
         string calldata _name,
         string calldata _email,
-        string calldata _password) external {
+        string calldata _password) external onlyAdmin {
             doctors[_address] = DoctorDetails(_address,
                                             _name,
                                             _email,
                                             _password);
-        grantRole(DOCTOR_ROLE, _address);
+        addDoctorRole(_address);
     }
     
 
-    function getPatient(address _address) public view returns (address, string memory, string memory, string memory, string memory){
-      if(patients[_address].addr == _address){
+    function getPatient(address _address) public view onlyDoctorPatient returns (address, string memory, string memory, string memory, string memory){
+      if(patients[_address].addr == msg.sender || isDoctor(msg.sender)){
         return (patients[_address].addr,
                 patients[_address].name,
                 patients[_address].email,
                 patients[_address].password,
                 patients[_address].patientHash);
        } 
-      revert('Patient does not exist');
+      revert('Patient does not exist or you do not have access to this record');
+       
+
     }
 
-    function getDoctor(address _address) public view returns (address, string memory, string memory, string memory){
+    function getDoctor(address _address) public view onlyAdmin returns(address, string memory, string memory, string memory){
         if(doctors[_address].addr == _address){
           return (doctors[_address].addr,
                 doctors[_address].name,
@@ -104,14 +135,14 @@ contract Ehr is AccessControl{
                           string memory _name,
                           string memory _email,
                           string memory _password,
-                          string memory _patientHash) public {
+                          string memory _patientHash) public onlyDoctor {
       patients[_address].name = _name;
       patients[_address].email = _email;
       patients[_address].password = _password;
       patients[_address].patientHash = _patientHash;
     }
 
-    function destroyPatient(address _address) public {
+    function destroyPatient(address _address) public onlyDoctor {
         delete patients[_address];
     }
 
--- test/ehr.test.js
+++ test/ehr.test.js
@@ -10,11 +10,13 @@ contract('Ehr', (accounts) => {
     let ehr;
     let admin;
     let doctor;
+    let patient;
 
     before(async () => {
         ehr = await Ehr.deployed();
         admin = accounts[0];
         doctor = accounts[1];
+        patient = accounts[2];
     });
     describe('deployment', async () => {
         it('Should deploy correctly', async () => {
@@ -36,9 +38,11 @@ contract('Ehr', (accounts) => {
 
     describe('new doctor', async () => {
         it('should add new doctor', async () => {
-            await ehr.newDoctor(admin, 'Doc', 'doc@email', 'docword');
-            const result = await ehr.getDoctor(admin);
-            assert.equal(result[0], admin);
+            await ehr.newDoctor(doctor, 'Doc', 'doc@email', 'docword', {
+                from: admin,
+            });
+            const result = await ehr.getDoctor(doctor);
+            assert.equal(result[0], doctor);
             assert.equal(result[1], 'Doc');
             assert.equal(result[2], 'doc@email');
             assert.equal(result[3], 'docword');
@@ -48,15 +52,15 @@ contract('Ehr', (accounts) => {
     describe('new patient', async () => {
         it('should add new patient', async () => {
             await ehr.newPatient(
-                accounts[2],
+                patient,
                 'Nigel',
                 'email',
                 'password',
                 'hash',
+                { from: doctor },
             );
-            const result = await ehr.getPatient(accounts[2]);
-            console.log(result);
-            assert.equal(result[0], accounts[2]);
+            const result = await ehr.getPatient(patient, { from: doctor });
+            assert.equal(result[0], patient);
             assert.equal(result[1], 'Nigel');
             assert.equal(result[2], 'email');
             assert.equal(result[3], 'password');
@@ -64,47 +68,173 @@ contract('Ehr', (accounts) => {
         });
     });
 
+    describe('Read patient', async () => {
+        it('Should return patient details', async () => {
+            const result = await ehr.getPatient(patient, { from: patient });
+            assert.equal(result[0], patient);
+            assert.equal(result[1], 'Nigel');
+            assert.equal(result[2], 'email');
+            assert.equal(result[3], 'password');
+            assert.equal(result[4], 'hash');
+        });
+    });
+
+    describe('Not read patient', async () => {
+        it('Should not return a patient record', async () => {
+            await ehr.newPatient(
+                accounts[4],
+                'Bob',
+                'bobmail',
+                'bobword',
+                'bobhash',
+                { from: doctor },
+            );
+            try {
+                await ehr.getPatient(patient, { from: accounts[4] });
+            } catch (e) {
+                assert(
+                    e.message.includes(
+                        'Patient does not exist or you do not have access to this record',
+                    ),
+                );
+            }
+        });
+    });
+
+    describe('Not new doctor', async () => {
+        it('should not add new doctor', async () => {
+            try {
+                await ehr.newDoctor(
+                    accounts[3],
+                    'Doc',
+                    'doc@email',
+                    'docword',
+                    {
+                        from: patient,
+                    },
+                );
+            } catch (e) {
+                assert(e.message.includes('Restricted to admins.'));
+            }
+            return;
+        });
+    });
+
+    describe('Not new patient', async () => {
+        it('should not add new patient', async () => {
+            try {
+                await ehr.newPatient(
+                    accounts[3],
+                    'Nigel',
+                    'email',
+                    'password',
+                    'hash',
+                    {
+                        from: patient,
+                    },
+                );
+            } catch (e) {
+                assert(e.message.includes('Restricted to doctors.'));
+            }
+            return;
+        });
+    });
+
     describe('update patient', async () => {
         it('should update patient', async () => {
-            await ehr.updatPatient(accounts[2], 'Frank', 'frankMail', 'pass');
-            const result = await ehr.getPatient(accounts[2]);
-            assert.equal(result[0], accounts[2]);
+            await ehr.updatePatient(
+                patient,
+                'Frank',
+                'frankMail',
+                'pass',
+                'hashish',
+                { from: doctor },
+            );
+            const result = await ehr.getPatient(accounts[2], { from: doctor });
+            assert.equal(result[0], patient);
             assert.equal(result[1], 'Frank');
             assert.equal(result[2], 'frankMail');
             assert.equal(result[3], 'pass');
+            assert.equal(result[4], 'hashish');
         });
     });
+    describe('Not update patient', async () => {
+        it('should not update patient', async () => {
+            try {
+                await ehr.updatePatient(
+                    patient,
+                    'Frank',
+                    'frankMail',
+                    'pass',
+                    'hashish',
+                    { from: patient },
+                );
+            } catch (e) {
+                assert(e.message.includes('Restricted to doctors.'));
+            }
+            return;
+        });
+    });
+
     describe('get a non-existing patient', async () => {
         it('should throw error when trying to read non-existing patient', async () => {
             try {
-                await ehr.getPatient(accounts[3]);
+                await ehr.getPatient(accounts[5], { from: doctor });
             } catch (e) {
-                assert(e.message.includes('Patient does not exist'));
-                return;
-            }
-            assert(false);
-        });
-    });
-    describe('destroy patient', async () => {
-        it('should destroy patient', async () => {
-            await ehr.destroyPatient(accounts[2]);
-            try {
-                await ehr.getPatient(accounts[2]);
-            } catch (e) {
-                assert(e.message.includes('Patient does not exist'));
+                console.log(e);
+                assert(
+                    e.message.includes(
+                        'Patient does not exist or you do not have access to this record',
+                    ),
+                );
                 return;
             }
         });
     });
 
-    describe('not destroy patient', async () => {
-        it('Should not destroy a non-existing patient', async () => {
+    describe('Not destroy patient', async () => {
+        it('should not destroy patient', async () => {
+            await ehr.newPatient(
+                accounts[4],
+                'Dan',
+                'danmail',
+                'danword',
+                'danhash',
+                { from: doctor },
+            );
             try {
-                await ehr.destroyPatient(accounts[2]);
+                await ehr.destroyPatient(patient, { from: accounts[4] });
             } catch (e) {
-                assert(e.message.includes('Patient does not exist'));
+                assert(e.message.includes('Restricted to doctors.'));
                 return;
             }
         });
     });
+
+    describe('destroy patient', async () => {
+        it('should destroy patient', async () => {
+            await ehr.destroyPatient(patient, { from: doctor });
+            try {
+                await ehr.getPatient(patient, { from: doctor });
+            } catch (e) {
+                assert(
+                    e.message.includes(
+                        'Patient does not exist or you do not have access to this record',
+                    ),
+                );
+                return;
+            }
+        });
+    });
+
+    // describe('not destroy patient', async () => {
+    //     it('Should not destroy a non-existing patient', async () => {
+    //         try {
+    //             await ehr.destroyPatient(patient, { from: doctor });
+    //         } catch (e) {
+    //             assert(e.message.includes('Patient does not exist'));
+    //             return;
+    //         }
+    //     });
+    // });
 });
